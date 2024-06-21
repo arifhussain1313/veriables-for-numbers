8e78a9df24aa5f37d56e07d1076f04422da310c0
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-20 22:33:21 -0700

    updated

--- main.js
+++ main.js
@@ -11,3 +11,10 @@ alert("You have visited this site " + visit + " times.");
 
 let birthyear =  "My birth year is 2001" + "\n" + "Data type of my declared variable is number";
 alert(birthyear);
+
+const visitor = "John Doe";
+const product = "T-Shirts";
+let quantity = 5;
+const message = '${visitor} ordered ${quantity} ${product}(s) on XYZ clothing store.';
+
+document.write(message);
